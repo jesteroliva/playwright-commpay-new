426c74c8b3e496ac190c315cbba5c2341e721167
jester <joliva@objectbright.com>
2026-01-25 00:53:36 +0800

    Initial Playwright setup

---
+++ actions/agencies_CRUD.ts
@@ -0,0 +1,33 @@
+import { test, expect} from '@playwright/test';
+import { login } from '../auth/creds';
+import type { Page, BrowserContext } from '@playwright/test';
+
+
+
+export async function agencies_CRUD({ page, context }: { page: Page; context: BrowserContext }) {
+ const AgencyName =  `test_Jester${Date.now()}`
+  await context.tracing.start({ screenshots: true, snapshots: true });
+  await login({ page });
+  const page2Promise = page.waitForEvent('popup');
+  await page.getByRole('link', { name: 'Agencies', exact: true }).click();
+  const page2 = await page2Promise;
+  await page2.waitForLoadState('domcontentloaded');
+  await page2.getByRole('button', { name: 'New Agency' }).click();
+  await page2.getByRole('textbox', { name: 'Agency Name *' }).fill(AgencyName);
+  await page2.getByRole('button', { name: 'Submit' }).click();
+  await expect(page2.getByRole('cell', { name: AgencyName })).toBeVisible();
+  await page2.getByRole('cell', { name: AgencyName }).click();
+  await page2.getByRole('button', { name: 'Edit' }).click();
+  await page2.getByRole('textbox', { name: 'Agency Name *' }).click();
+  await page2.getByRole('textbox', { name: 'Agency Name *' }).fill('TEST QA AGENCY 123');
+  await page2.getByRole('button', { name: 'Update' }).click();
+  await expect(page2.getByRole('cell', { name: 'TEST QA AGENCY 123' })).toBeVisible();
+  await page2.getByRole('cell', { name: 'TEST QA AGENCY 123' }).click();
+  await page2.getByRole('cell', { name: 'TEST QA AGENCY 123' }).click();
+  //await page2.getByRole('button').nth(4).click();
+  await page2.getByRole('button', { name: '' }).click();
+  await page2.getByRole('button', { name: 'Delete' }).click();
+  await expect(page2.getByRole('cell', { name: 'TEST QA AGENCY 123'})).not.toBeVisible();
+  await context.tracing.stop({ path: 'traceAgencies.zip' });
+  await page.close();
+}
---
+++ auth/creds.ts
@@ -0,0 +1,15 @@
+import  {Page} from 'playwright';
+
+export async function login({ page }: { page: Page }){
+  await page.goto('https://commpay-dev.commtpa.com/login');
+  const page1Promise = page.waitForEvent('popup');
+  await page.getByRole('link', { name: 'Logo Sign in with LTC Account' }).click();
+  const page1 = await page1Promise;
+  await page1.getByRole('textbox', { name: 'Email' }).click();
+  await page1.getByRole('textbox', { name: 'Email' }).fill(process.env.EMAIL!);
+  await page1.getByRole('textbox', { name: 'Password' }).click();
+  await page1.getByRole('textbox', { name: 'Password' }).fill(process.env.PASSWORD!);
+  await page1.getByRole('button', { name: 'Log in' }).click();
+  await page.goto('https://commpay-dev.commtpa.com/dashboard');
+  await page.getByRole('link', { name: 'Manage LION Client' }).click();
+}
