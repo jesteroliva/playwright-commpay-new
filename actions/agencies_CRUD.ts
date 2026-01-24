391d7fcd5a6aad5923762dfba18ddb9459affb5f
jester <joliva@objectbright.com>
2026-01-25 02:17:56 +0800

    adding another fixes pt3

--- actions/agencies_CRUD.ts
+++ actions/agencies_CRUD.ts
@@ -7,14 +7,18 @@ import type { Page, BrowserContext } from '@playwright/test';
 export async function agencies_CRUD({ page, context }: { page: Page; context: BrowserContext }) {
  const AgencyName =  `test_Jester${Date.now()}`
   await context.tracing.start({ screenshots: true, snapshots: true });
+  await console.log('Step1: login')
   await login({ page });
   const page2Promise = page.waitForEvent('popup');
+  await console.log('Clicking agencies')
   await page.getByRole('link', { name: 'Agencies', exact: true }).click();
   const page2 = await page2Promise;
   await page2.waitForLoadState('domcontentloaded');
+  await console.log('New agency')
   await page2.getByRole('button', { name: 'New Agency' }).click();
   await page2.getByRole('textbox', { name: 'Agency Name *' }).fill(AgencyName);
   await page2.getByRole('button', { name: 'Submit' }).click();
+  await console.log('Add success')
   await expect(page2.getByRole('cell', { name: AgencyName })).toBeVisible();
   await page2.getByRole('cell', { name: AgencyName }).click();
   await page2.getByRole('button', { name: 'Edit' }).click();
